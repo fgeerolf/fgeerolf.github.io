ed960bb06e0f8618558dc8965be79115bc965895
François Geerolf <geerolf@wifi-131-179-37-162.host.ucla.edu>
2019-02-02 16:28:05 -0800

    updating

--- code/irs.R
+++ code/irs.R
@@ -18,20 +18,17 @@ names <- c("11incyallagi", "irs_2011",
 # IRS (2011 - 2016) ----------
 
 for (i in 1:nrow(names)){
-  cat("\nDownloading", names[i, 1], "and saving as", names[i, 2], "\n")
+  cat("Downloading", names[i, 1], "and saving as", names[i, 2], "\n")
   assign(names[i, 2], names[i, 1] %>%
            paste0("https://www.irs.gov/pub/irs-soi/", ., ".csv") %>%
            fread)
   do.call(save, list(names[i, 2], file = paste0(names[i, 2], ".RData")))
 }
 
-irs_2011 %>% str
-irs_2011 %>% head
-
 # Zipcode (2005 - 2016) ----------
 
 for (year in 2005:2016){
-  cat("\nDownloading Zipcode from year", year, "\n")
+  cat("Downloading Zipcode from year", year, "\n")
   assign(paste0("zipcode_", year), paste0(year) %>%
            paste0("http://www.nber.org/tax-stats/zipcode/", ., "/zipcode", ., ".csv") %>%
            fread)
