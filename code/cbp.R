5e38d72317e9cba4decc899595d9e04aa00315ba
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-03 16:15:52 -0800

    updating

---
+++ code/cbp.R
@@ -0,0 +1,28 @@
+rm(list = ls())
+pklist <- c("curl", "tidyverse", "benchmarkme", "pryr")
+source("https://fgeerolf.github.io/code/load-packages.R")
+
+cat("Model:", get_cpu()$model_name, 
+    "\nMemory:", round(get_ram()/2^30, digits = 3), 
+    "Go\nNumber of cores:", get_cpu()$no_of_cores, 
+    "\nStart: ", format(Sys.time(), "%a %b %d, %Y - %X"), "\n")
+
+for (year in 1986:2016){
+  yearD <- year %>% paste %>% substr(3, 4)
+  
+  cat("CBP", year, ": Currently downloading...")
+  curl_download(paste0("https://www2.census.gov/programs-surveys/cbp/datasets/", year, "/cbp", yearD, "co.zip"), 
+                destfile = paste0("cbp", yearD, "co.zip"), 
+                quiet = FALSE, 
+                mode = "wb")
+  unzip(paste0("cbp", yearD, "co.zip"))
+  unlink(paste0("cbp", yearD, "co.zip"))
+  
+  cat(" Reading...")
+  assign(paste0("cbp_", year), fread(paste0("cbp", yearD, "co.txt")))
+  unlink(paste0("cbp", yearD, "co.txt"))
+  
+  cat(" Saving...\n")
+  save(list = paste0("cbp_", year), file = paste0("cbp_", year, ".RData"))
+  rm(list = paste0("cbp_", year))
+}
