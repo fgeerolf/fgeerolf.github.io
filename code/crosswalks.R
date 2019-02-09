29a1e1149f8c53bd3845d6fb6536320468821adf
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-08 19:28:41 -0800

    updating

--- code/crosswalks.R
+++ code/crosswalks.R
@@ -1,6 +1,5 @@
 rm(list = ls())
-pklist <- c("tidyverse", "curl", "fredr", "data.table", 
-            "pryr", "benchmarkme", "readstata13")
+pklist <- c("tidyverse", "curl", "data.table", "pryr", "benchmarkme", "readstata13")
 source("https://fgeerolf.github.io/code/load-packages.R")
 
 cat("Model:", get_cpu()$model_name, 
@@ -15,8 +14,6 @@ zipcode_to_county <- "https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_c
   mutate(fips = STATE*1000+COUNTY) %>%
   select(zipcode = ZCTA5, fips) %>%
   arrange(zipcode)
-
-zipcode_to_county %>% str
 
 save(zipcode_to_county, file = "zipcode_to_county.RData")
 
@@ -58,3 +55,22 @@ unlink("cw_czone_division.zip")
 cz_to_census_division <- read.dta13("cw_czone_division.dta")
 unlink("cw_czone_division.dta")
 save(cz_to_census_division, file = "cz_to_census_division.RData")
+
+# cbsa_to_county ----------
+
+cbsa_to_county <- "http://www.nber.org/cbsa-msa-fips-ssa-county-crosswalk/cbsatocountycrosswalk.csv" %>% 
+  fread
+save(cbsa_to_county, file = "cbsa_to_county.RData")
+
+# cbsa_to_county_2017 ----------
+
+cbsa_to_county_2017 <- "https://www.nber.org/cbsa-msa-fips-ssa-county-crosswalk/2017/cbsatocountycrosswalk2017.csv" %>% 
+  fread
+save(cbsa_to_county_2017, file = "cbsa_to_county_2017.RData")
+
+# cbsa_to_fips ----------
+
+cbsa_to_fips <-  "https://www.nber.org/cbsa-csa-fips-county-crosswalk/cbsa2fipsxw.csv" %>%
+  fread
+
+save(cbsa_to_fips, file = "cbsa_to_fips.RData")
