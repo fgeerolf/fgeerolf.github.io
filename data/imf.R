047affd60e6b348f288679b185156d10d06f661c
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-10 18:58:18 -0800

    updating

--- data/imf.R
+++ data/imf.R
@@ -1,4 +1,4 @@
-pklist <- c("curl", "tidyverse", "rvest", "data.table", "benchmark")
+pklist <- c("curl", "tidyverse", "rvest", "data.table", "benchmarkme")
 source("https://fgeerolf.github.io/code/load-packages.R")
 
 cat("Model:", get_cpu()$model_name,
