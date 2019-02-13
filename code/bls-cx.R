84319a3d0235203372d9fdde4cd98b58262de21a
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-12 19:43:20 -0800

    updating

---
+++ code/bls-cx.R
@@ -0,0 +1,27 @@
+rm(list = ls())
+pklist <- c("tidyverse", "curl", "fredr", "data.table", 
+            "pryr", "benchmarkme", "rvest")
+source("https://fgeerolf.github.io/code/load-packages.R")
+
+cat("Model:", get_cpu()$model_name, 
+    "\nMemory:", round(get_ram()/2^30, digits = 3), 
+    "Go\nNumber of cores:", get_cpu()$no_of_cores, 
+    "\nStart: ", format(Sys.time(), "%a %b %d, %Y - %X"), "\n")
+
+datasets <- read_html("https://download.bls.gov/pub/time.series/cx/") %>% 
+  html_nodes("a") %>%
+  html_text(trim = TRUE) %>%
+  as.data.frame %>%
+  rename(X0 = ".") %>%
+  cbind(read_html("https://download.bls.gov/pub/time.series/cx/") %>%
+          str_match_all("<a href=\"(.*?)\"") %>%
+          as.data.frame %>%
+          mutate(X2 = paste0("https://download.bls.gov", X2))) %>%
+  mutate_all(paste)
+
+for (i in 2:11){
+  file <- datasets[i, "X0"]
+  cat("\nDownloading from BLS Website CEX:", file)
+  assign(file, read.csv(datasets[i, "X2"], sep = "\t"))
+  do.call(save, list(file, file = paste0(file, ".RData")))
+}
