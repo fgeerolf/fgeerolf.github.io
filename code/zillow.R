57a2b1d4bb7f9fa5191ff91936e6c1928e670f82
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-15 00:36:32 -0800

    updating

--- code/zillow.R
+++
@@ -1,5 +0,0 @@
-source("https://fgeerolf.github.io/data/zillow-city.R")
-source("https://fgeerolf.github.io/data/zillow-county.R")
-source("https://fgeerolf.github.io/data/zillow-metro.R")
-source("https://fgeerolf.github.io/data/zillow-zip.R")
-source("https://fgeerolf.github.io/data/zillow-crosswalk.R")
