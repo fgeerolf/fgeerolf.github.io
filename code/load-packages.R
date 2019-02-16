b9dbee9c6372bf82813f34c4bfaccfbf2cd537cf
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-15 18:43:44 -0800

    updating

--- code/load-packages.R
+++
@@ -1,7 +0,0 @@
-pack.fun <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]; 
-  if (length(new.pkg)) install.packages(new.pkg, dependencies = TRUE, repos='http://cran.us.r-project.org'); 
-  sapply(pkg, require, character.only = TRUE)
-}
-
-if (exists("pklist")) pack.fun(pklist); rm(pklist,pack.fun)
