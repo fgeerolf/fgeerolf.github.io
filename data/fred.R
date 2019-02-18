174b52c8b19fb685f077f3b286f6a3a32ef06d27
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-18 10:33:37 -0800

    updating

---
+++ data/fred.R
@@ -0,0 +1,13 @@
+rm(list = ls())
+pklist <- c("tidyverse", "fredr", "lubridate")
+source("https://fgeerolf.github.io/code/load-packages.R")
+
+fredr_set_key("564a4e78cdf46ceeaf913616a77a13cb")
+
+fred <- map_dfr(c("UNRATE", 
+                  "FEDFUNDS",
+                  "CSUSHPINSA",
+                  "MDSP"), 
+                fredr)
+
+save(fred, file = "fred.RData")
