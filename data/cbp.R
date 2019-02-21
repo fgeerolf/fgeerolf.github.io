f8d392df3eec766381cc40314f6ea166644bda6c
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-21 10:55:22 -0800

    updating

---
+++ data/cbp.R
@@ -0,0 +1,28 @@
+pklist <- c("curl", "tidyverse", "data.table")
+source("http://fgeerolf.com/code/load-packages.R")
+
+for (year in 1986:2016){
+  yearD <- year %>% paste %>% substr(3, 4)
+  
+  cat("CBP", year, ": Currently downloading...\n")
+  curl_download(paste0("https://www2.census.gov/programs-surveys/cbp/datasets/", year, "/cbp", yearD, "co.zip"), 
+                destfile = paste0("cbp", yearD, "co.zip"), 
+                quiet = FALSE, 
+                mode = "wb")
+  unzip(paste0("cbp", yearD, "co.zip"))
+  unlink(paste0("cbp", yearD, "co.zip"))
+  
+  cat("Reading...")
+  
+  if (!year %in% c(2002, 2007, 2008)){
+    assign(paste0("cbp_", year), fread(paste0("cbp", yearD, "co.txt")))
+    unlink(paste0("cbp", yearD, "co.txt"))
+  } else{
+    assign(paste0("cbp_", year), fread(paste0("Cbp", yearD, "co.txt")))
+    unlink(paste0("Cbp", yearD, "co.txt"))
+  }
+  
+  cat(" Saving...\n")
+  save(list = paste0("cbp_", year), file = paste0("cbp_", year, ".RData"))
+  rm(list = paste0("cbp_", year))
+}
