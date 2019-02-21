9576ded777bae44498694217e624d651dabb2d1a
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-21 11:05:04 -0800

    updating

---
+++ data/elasticity.R
@@ -0,0 +1,25 @@
+pklist <- c("tidyverse", "curl", "foreign")
+source("http://fgeerolf.com/code/load-packages.R")
+
+# housing_supply ---------
+
+curl_download("http://web.archive.org/web/20100619052721/http://real.wharton.upenn.edu/~saiz/SUPPLYDATA.zip", 
+              destfile = "SUPPLYDATA.zip", 
+              quiet = FALSE)
+unzip("SUPPLYDATA.zip")
+housing_supply <- read.dta("HOUSING_SUPPLY.dta")
+unlink(c("HOUSING_SUPPLY.dta", "SUPPLYDATA.zip", "readme.txt"))
+
+save(housing_supply, file = "housing_supply.RData")
+
+# wharton_land_regulation ---------
+
+curl_download("http://real.wharton.upenn.edu/~gyourko/WRLURI/WHARTON%20LAND%20REGULATION%20DATA_1_24_2008.zip", 
+              destfile = "WHARTON LAND REGULATION DATA_1_24_2008.zip", 
+              quiet = FALSE)
+
+unzip("WHARTON LAND REGULATION DATA_1_24_2008.zip")
+wharton_land_regulation <- read.dta("WHARTON LAND REGULATION DATA_1_24_2008.dta")
+unlink(c("WHARTON LAND REGULATION DATA_1_24_2008.zip", "WHARTON LAND REGULATION DATA_1_24_2008.dta"))
+
+save(wharton_land_regulation, file = "wharton_land_regulation.RData")
