0c13e06d2868f6d6e78737a17b9be8ef2ee7a8d7
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-21 10:59:31 -0800

    updating

---
+++ data/bls-jt.R
@@ -0,0 +1,22 @@
+pklist <- c("tidyverse", "curl", "data.table", "rvest")
+source("http://fgeerolf.com/code/load-packages.R")
+
+datasets <- read_html("https://download.bls.gov/pub/time.series/jt/") %>% 
+  html_nodes("a") %>%
+  html_text(trim = TRUE) %>%
+  as.data.frame %>%
+  rename(X0 = ".") %>%
+  cbind(read_html("https://download.bls.gov/pub/time.series/jt/") %>%
+          str_match_all("<a href=\"(.*?)\"") %>%
+          as.data.frame %>%
+          mutate(X2 = paste0("https://download.bls.gov", X2))) %>%
+  mutate_all(paste)
+
+datasets %>% as_tibble %>% print
+
+for (i in 2:18){
+  file <- datasets[i, "X0"]
+  cat("Downloading from BLS Website JOLTS:", file, "\n")
+  assign(file, datasets[i, "X2"] %>% fread)
+  do.call(save, list(file, file = paste0(file, ".RData")))
+}
