4b764698b76c007f7403bf2ad7bc4a6b3713c495
François Geerolf <geerolf@Francoiss-MacBook-Pro.local>
2019-02-22 13:14:04 -0800

    updating

--- data/shiller.R
+++ data/shiller.R
@@ -6,6 +6,11 @@ source("http://fgeerolf.com/code/load-packages.R")
 shiller <- "http://www.econ.yale.edu/~shiller/data/ie_data.xls" %>%
   read.xls(skip = 6) %>%
   head(-1) %>% # remove last obs
-  mutate(CPI = CPI %>% as.numeric(CPI))
+  mutate_all(as.numeric) %>%
+  mutate(year = Date %>% round,
+         month = ((Date - year)*100) %>% round,
+         date = paste0(year, "-", str_pad(month, 2, pad = "0"), "-01") %>% as.Date) %>%
+  select(-year, -month, -Date) %>%
+  select(date, everything())
 
 save(shiller, file = "shiller.RData")
